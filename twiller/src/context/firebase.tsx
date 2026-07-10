eb3b8e1c55ea141161cfba666c0e5db5aef4e727
Vaibhav Sharma <vaibhavhimanshusharma@gmail.com>
2026-07-10 22:53:39 +0530

    changes

--- twiller/src/context/firebase.tsx
+++ twiller/src/context/firebase.tsx
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // keep your credentials 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyAeV28Q3C0b4aLzrSHosK9VmEc09OtFULs",
+  authDomain: "twiller-b3309.firebaseapp.com",
+  projectId: "twiller-b3309",
+  storageBucket: "twiller-b3309.firebasestorage.app",
+  messagingSenderId: "457143345315",
+  appId: "1:457143345315:web:afcec2fbba453c97a85f16",
+  measurementId: "G-9Z6KWLMS1Q"
 };
 
 
